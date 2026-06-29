a42c92282eb87854690542b1850d7fd33101de24
Kelvin Ifeka <kelvin.ifeka@stu.cu.edu.ng>
2026-06-29 18:06:11 +0100

    Commit 8: Introduce Cell topology

--- crates/mesh/src/lib.rs
+++ crates/mesh/src/lib.rs
@@ -2,8 +2,10 @@ pub mod node_id;
 pub mod node;
 pub mod edge;
 pub mod face;
+pub mod cell;
 
 pub use node_id::NodeId;
 pub use node::Node;
 pub use edge::Edge;
 pub use face::Face;
+pub use cell::Cell;
